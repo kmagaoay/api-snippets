e8667576aeabc7540fa5780797db0e68bd15659c
kmagaoay <kmagaoay@users.noreply.github.com>
2017-06-21 23:03:28 -0700

    Update instance-get-example-1.6.x.py

--- rest/queue/instance-get-example-1/instance-get-example-1.6.x.py
+++ rest/queue/instance-get-example-1/instance-get-example-1.6.x.py
@@ -2,8 +2,8 @@
 from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-auth_token = "your_auth_token"
+account_sid = "AC1a7339529e4074af182f7ce55aa7ac17"
+auth_token = "694981e351f9e86d3d908ebdc889ec89"
 client = Client(account_sid, auth_token)
 
 queue = client.queues("QU5ef8732a3c49700934481addd5ce1659") \
